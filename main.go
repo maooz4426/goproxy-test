a9428cdd73f7e802c063a147a947b92a7b789fbc
MAOZ <maooz4426@gmail.com>
2025-08-25 16:57:18 +0900

    fix

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-
-	"github.com/maooz4426/hello"
-)
-
 func main() {
-	mes, err := hello.Hello("maoz")
-	log.Println(err)
-	fmt.Println(mes)
+	//mes, err := hello.Hello("maoz")
+	//log.Println(err)
+	//fmt.Println(mes)
 }
